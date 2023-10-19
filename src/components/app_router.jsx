1c10e3c8c4e17e2ada417369935f697715985619
Alan <alan.cabezas@usm.cl>
2023-10-19 16:28:14 -0300

    fix

--- src/components/app_router.jsx
+++ src/components/app_router.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home'
-import InternalPage from '../pages/internal'
 import PMVComponent from '../pages/mindguardian'
 import Formulario from '../pages/formulario'
 import NatureQuest from '../pages/naturequest'
@@ -13,7 +12,7 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/internal' element={<Formulario />} />
+        <Route path='/formulario' element={<Formulario />} />
         <Route path='/mindguardian' element={<PMVComponent/>} />
         <Route path='/naturequest' element={<NatureQuest/>} />
         <Route path='/safesym' element={<SafeSym/>} />
