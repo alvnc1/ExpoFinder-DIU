7c1a623e7d6a55f163afd93eb52208393042b846
jofloress <josefa.flores@sansano.usm.cl>
2023-10-19 20:16:52 -0300

    update subir_proyecto

--- src/components/app_router.jsx
+++ src/components/app_router.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home'
 import PMVComponent from '../pages/mindguardian'
-import Formulario from '../pages/formulario'
+import Formulario from '../pages/subir_proyecto'
 import NatureQuest from '../pages/naturequest'
 import SafeSym from '../pages/safesym'
 
@@ -12,7 +12,7 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/formulario' element={<Formulario />} />
+        <Route path='/subir_proyecto' element={<Formulario />} />
         <Route path='/mindguardian' element={<PMVComponent/>} />
         <Route path='/naturequest' element={<NatureQuest/>} />
         <Route path='/safesym' element={<SafeSym/>} />
