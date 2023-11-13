61a68c0515ac9ccc714cc6d7959320a472f1d37a
Alan <alan.cabezas@usm.cl>
2023-11-13 18:11:19 -0300

    a

--- src/pages/figmaform.jsx
+++ src/pages/figmaform.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import backgroundImage from '../assets/fesw-logo-w.png';
 import NavBar from '../components/nav_bar'
 
 export const FigmaForm = () => {
   return (
+    
     <><><div style={{
           backgroundColor: '#D9D9D9',
           height: '12vh',
